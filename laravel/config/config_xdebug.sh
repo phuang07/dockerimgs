3073f303e33ea723225d55e712795d933c88c649
Ray Huang <leoman730@gmail.com>
2015-05-18 12:40:44 -0400

    Add xdebug and rsyslogd to supervisor config

--- laravel/config/config_xdebug.sh
+++ laravel/config/config_xdebug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -f /xdebug_configured ]; then
+if [ ! -f /xdebug_configured ]; then	
     echo "=> Xdebug is not configured yet, configuring Xdebug ..."
     DOCKER_HOST_IP=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
     echo "xdebug.remote_enable=on" >> /etc/php5/mods-available/xdebug.ini
